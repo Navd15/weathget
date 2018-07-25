ba3f0aa2f0886485a6fc986987fe2621d384b255
Navd15 <gill.222navdeep@gmail.com>
2018-07-25 23:54:16 +0530

    added nav menu

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const {app,BrowserWindow}= require('electron');
+const electron =require('electron')
 const fs=require('fs');
 let win;
 
-
 function createWidget(){
-    // console.log(screen);
-win=new BrowserWindow({skipTaskbar:true,alwaysOnTop:false,hasShadow:true,width:400,height:300,resizable:true,frame:false,webPreferences:{
-    devTools:false
+     console.log(electron.screen);
+win=new BrowserWindow({skipTaskbar:false,alwaysOnTop:false,hasShadow:true,width:400,height:350,resizable:true,frame:true,webPreferences:{
+    devTools:true
 }})
 win.loadFile('./index.html')
 
---
+++ res/anim/jsanim.js
@@ -0,0 +1,13 @@
+$(document).ready(
+function cs(){
+
+     let slider=$('#slider');
+
+    $('#settings').click(()=>{
+        console.log("f");
+        $(this).toggleClass('forSet')  
+        $(slider).toggleClass("toLeft")
+
+
+}
+)})
