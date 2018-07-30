7ba07c92c23ce300437e26387d2a124365297e48
Navd15 <gill.222navdeep@gmail.com>
2018-07-30 23:42:24 +0530

    added tray icon

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-const {app,BrowserWindow}= require('electron');
-const fs=require('fs');
+const {ipcMain,MenuItem,app,BrowserWindow,Tray,Menu}= require('electron');
+
+
 let win;
+let tray;
+
+
+function tra(){
+    tray=new Tray('./res/img/tray.ico');
+    const cmenu=Menu.buildFromTemplate([
+{label:'About',type:'normal'},
+{label:'Settings',type:'normal'}
+    ]);
+    tray.setContextMenu(cmenu);
+    tray.setToolTip('More');
+}
+
 
 function createWidget(){
-     console.log(electron.screen);
 win=new BrowserWindow({skipTaskbar:false,alwaysOnTop:false,hasShadow:true,width:400,height:350,resizable:true,frame:true,webPreferences:{
     devTools:true
 }})
+tra();
 win.loadFile('./index.html')
 
 }
 
 
 app.on('ready',createWidget)
+
