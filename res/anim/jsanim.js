61faa2bf1eafc06a5790e02db682a861c4c578f6
Navd15 <gill.222navdeep@gmail.com>
2018-08-17 23:48:22 +0530

    add main view ,YantraManav fontfamily

--- res/anim/jsanim.js
+++ res/anim/jsanim.js
@@ -1,34 +1,41 @@
-$( document ).ready(
-    function(){
-        let slider=$('#slider');
-        let set=$('#settings');
+$(document).ready(
 
-        set.on('hover',()=>{
-$(this).css({'opacity':1});
+
+    function () {
+        let slider = $('#slider');
+        let set = $('#sett');
+
+        set.on('hover', () => {
+            $(this).css({
+                'opacity': 1
+            });
 
         })
-       set.click(function(){
+        set.click(function () {
 
-        $('.noLocMsg').css({"filter":"blur(3px)"})
-slider.animate({
-left:"0px"
-
-
-},100);
-   
-   
-   }
-   )
-
-   $('.noLocMsg').on('click',()=>{
-    $('.noLocMsg').css({"filter":''});
-    slider.animate({
-left:'-140px'
-
-    },100)
-})
+            $('.noLocMsg').css({
+                "filter": "blur(3px)"
+            })
+            slider.animate({
+                left: "0px"
 
 
+            }, 100);
 
 
-})
+        })
+
+        $('#replacer').on('click', () => {
+            $('.noLocMsg').css({
+                "filter": ''
+            });
+            slider.animate({
+                left: '-140px'
+
+            }, 100)
+        })
+
+
+
+
+    })
