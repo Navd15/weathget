33ebd54fcb0cdd4ef2e5d676992168d6930018d8
Navd15 <gill.222navdeep@gmail.com>
2018-09-08 23:13:50 +0530

    changed weather api to apixu from accuweather

--- helperJS/helper.js
+++ helperJS/helper.js
@@ -5,11 +5,15 @@
  */
 
  /* Class contains keys and stuff for API calling */
+ let replacer;
 class creds{
-    static Keys=()=>{
-return {key : 'A2V9GFbwbk8jX6F2V1jua6KcHKtAy7on',
-initialURL : `http://dataservice.accuweather.com`,
-'12HourApi' : `/forecasts/v1/hourly/12hour/`,
+    static Keys(){
+return {
+    key:`a2aa7c3e0225461dada105616172206`,
+    // key : 'A2V9GFbwbk8jX6F2V1jua6KcHKtAy7on',
+// initialURL : `http://dataservice.accuweather.com`,
+initialURL:`http://api.apixu.com/v1`,
+'12HourApi' : `/forecast.json`,
 
 }
     }
@@ -20,11 +24,24 @@ list from html page---
 */
 function setListClick(id) { 
     let key=creds.Keys().key;
-    let url = `/forecasts/v1/daily/1day/${id}?apikey=${key}&details=false&metric=true`
+    let initialURL=creds.Keys().initialURL;
+    let url = `/current.json?key=${key}&q=${id}`
     fetch(`${initialURL}/${url}`).then((resolve) => {
         resolve.json().then((resolve) => {
-            module.exports.info=resolve;
-            console.log(resolve)
+
+ new FutureCast(id,initialURL,key).castRes().then((hourly)=>{
+
+                let whole={ currentCond:resolve,
+        hourCond:hourly
+                }
+
+fill.makeHTML(whole,(res)=>{
+replacer.innerHTML=res;
+})
+            })
+           
+
+            
         })
 
     }, (reject) => {
@@ -36,10 +53,7 @@ function setListClick(id) {
 
 
 $(document).ready(function () {
-
-
-    let replacer=document.getElementById('replacer');
-    
+ replacer=document.getElementById('replacer');
 let addLocBut=$('#but_addLoc');
 addLocBut.on('click',()=>{
     console.log(addLoc.material())
@@ -55,8 +69,7 @@ addLocBut.on('click',()=>{
             let key=creds.Keys().key;
             let initialURL=creds.Keys().initialURL;
             return new Promise((resolve, rej) => {
-                let url = `${initialURL}/locations/v1/cities/autocomplete?apikey=${key}&q=${loc}`;
-
+                let url = `${initialURL}/search.json?key=${key}&q=${loc}`;
                 fetch(url).then(
                     (response) => {
                         if (response.status == 200)
@@ -86,7 +99,6 @@ function exploit(){
             loading.css('display','block')
             if (searchBar.value != undefined) {
                 searchLoc(searchBar.value).then((res) => {
-                    fill.s(res)
                     gen.genList(res, locUl).then((res)=>{
                        if(res==true){
                       loading.css('display','none')
@@ -108,12 +120,45 @@ function exploit(){
 
 
 class FutureCast{
-    static castRes=(key,initialURL)=>{
-let url=`${initialURL}`
-        return fetch().then((resolve)=>{
+    
+    constructor(cityId,initialURL,key){
+        this.cityId=cityId;
+this.initialURL=initialURL;
+this.key=key;
+this.api=creds.Keys()["12HourApi"];
+    }
 
-})
+    /* For next 12 hours forecast */
+
+     castRes(){
+
+let url=`${this.initialURL}${this.api}?key=${this.key}&q=${this.cityId}`;
+
+        return new Promise((outResolve)=>{
+
+            fetch(url).then((resolve)=>{
+                if (resolve.status==200){
+                resolve.json().then((res)=>{
+outResolve(res);
+
+                })
+
+                }
+                
+                
+                
+                })
+
+        })
+        
+       
 
 
     }
+weekCast(callback){
+
+/* For weeklyForCast */
+}
+
+
 }
