22e416193f25e4dfc6d521070409d9f04ef50061
Navd15 <gill.222navdeep@gmail.com>
2018-09-15 07:58:21 +0530

    if-else fix for tray in linux and macos

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
-const {ipcMain,MenuItem,app,BrowserWindow,Tray,Menu}= require('electron');
-
+const {
+    ipcMain,
+    MenuItem,
+    app,
+    BrowserWindow,
+    Tray,
+    Menu
+} = require('electron');
+const os = require('os');
 
 let win;
 let tray;
 
+function createWidget() {
+    win = new BrowserWindow({
+        skipTaskbar: false,
+transparent:true,
+        alwaysOnTop: false,
+        hasShadow: true,
+        movable:true,
+        width: 500,
+        height: 290,
+        resizable: true,
+        frame:false,
+        webPreferences: {
+            devTools: true
+        }
+    })
+    console.log(os.platform());
+    
+    if(os.platform=='win32'){
+    tra();
+    }
+    
+    win.loadFile('./index.html')
 
-function tra(){
-    tray=new Tray('./res/img/tray.ico');
-    const cmenu=Menu.buildFromTemplate([
-{label:'About',type:'normal'},
-{label:'Settings',type:'normal'}
+}
+
+function tra() {
+    tray = new Tray('./res/img/tray.ico');
+    const cmenu = Menu.buildFromTemplate([{
+            label: 'About',
+            type: 'normal'
+        },
+        {
+            label: 'Settings',
+            type: 'normal'
+        }
     ]);
     tray.setContextMenu(cmenu);
     tray.setToolTip('More');
 }
 
 
-function createWidget(){
-win=new BrowserWindow({skipTaskbar:false,alwaysOnTop:false,hasShadow:true,width:400,height:350,resizable:true,frame:true,webPreferences:{
-    devTools:true
-}})
-tra();
-win.loadFile('./index.html')
-
-}
-
-
-app.on('ready',createWidget)
-
+app.on('ready', createWidget)
