728fad1218a0605cc5e6f2658b25a40a5ecae723
Navd15 <gill.222navdeep@gmail.com>
2018-09-30 18:44:15 +0530

    fix:Changed pattern to spa ;added methods for slider location search

--- helperJS/helper.js
+++ helperJS/helper.js
@@ -1,47 +1,46 @@
-/*
- *
- * Created by navcer on 01-08-2018.
- *
- */
+'use-strict';
 
- /* Class contains keys and stuff for API calling */
- let replacer;
-class creds{
-    static Keys(){
-return {
-    key:`a2aa7c3e0225461dada105616172206`,
-    // key : 'A2V9GFbwbk8jX6F2V1jua6KcHKtAy7on',
-// initialURL : `http://dataservice.accuweather.com`,
-initialURL:`http://api.apixu.com/v1`,
-'12HourApi' : `/forecast.json`,
+let replacer;
 
-}
+/* Class contains keys and stuff for API calling */
+class Creds {
+
+    static Keys() {
+        return {
+            key: `a2aa7c3e0225461dada105616172206`,  // Replace with your apixu API key | www.apixu.com
+            // key : 'A2V9GFbwbk8jX6F2V1jua6KcHKtAy7on',
+            // initialURL : `http://dataservice.accuweather.com`,
+            initialURL: `http://api.apixu.com/v1`,
+            '12HourApi': `/forecast.json`,
+
+        }
     }
 }
 
-/* ---This method triggeres when user selects the location from 
-list from html page---
+/* This method triggeres when user selects the location from 
+list from html page
 */
-function setListClick(id) { 
-    let key=creds.Keys().key;
-    let initialURL=creds.Keys().initialURL;
+function setListClick(id) {
+    let key = Creds.Keys().key;
+    let initialURL = Creds.Keys().initialURL;
     let url = `/current.json?key=${key}&q=${id}`
     fetch(`${initialURL}/${url}`).then((resolve) => {
         resolve.json().then((resolve) => {
 
- new FutureCast(id,initialURL,key).castRes().then((hourly)=>{
+            new FutureCast(id, initialURL, key).castRes().then((hourly) => {
 
-                let whole={ currentCond:resolve,
-        hourCond:hourly
+                let whole = {
+                    currentCond: resolve,
+                    hourCond: hourly
                 }
 
-fill.makeHTML(whole,(res)=>{
-replacer.innerHTML=res;
-})
+                fill.makeHTML(whole, (res) => {
+                    replacer.innerHTML = res;
+                })
             })
-           
 
-            
+
+
         })
 
     }, (reject) => {
@@ -53,112 +52,91 @@ replacer.innerHTML=res;
 
 
 $(document).ready(function () {
- replacer=document.getElementById('replacer');
-let addLocBut=$('#but_addLoc');
-addLocBut.on('click',()=>{
-    console.log(addLoc.material())
-    replacer.innerHTML=addLoc.material()
-    exploit()
+    replacer = document.getElementById('replacer');
+     let search=$('#searchInput')[0];
+     console.log(search)
+    let go = $('#searchButton');
+     let locList=$('#locList')[0];
+     console.log(locList);
+     
+go.on('click',()=>{
+if(search.value!="")
+console.log(search.value);
+searchLoc(search.value).then((resolve)=>{
+ gen.genList(resolve,locList).then((resolve)=>{
+/* Hide loading animation  */
+if(resolve==true)
+$(locList).css('display', 'block')
+ })
 
 })
-    
-
-/* --------------------------------------------------------------- */
-
-        function searchLoc(loc){
-            let key=creds.Keys().key;
-            let initialURL=creds.Keys().initialURL;
-            return new Promise((resolve, rej) => {
-                let url = `${initialURL}/search.json?key=${key}&q=${loc}`;
-                fetch(url).then(
-                    (response) => {
-                        if (response.status == 200)
-                            response.json().then((locations) => {
-                                resolve(locations)
-
-                            })
+})
 
 
+    /* --------------------------------------------------------------- */
+
+    function searchLoc(loc) {
+        let key = Creds.Keys().key;
+        let initialURL = Creds.Keys().initialURL;
+        return new Promise((resolve, err) => {
+            let url = `${initialURL}/search.json?key=${key}&q=${loc}`;
+            fetch(url).then(
+                (response) => {
+                    if (response.status == 200){
+                        response.json().then((locations) => {
+                            resolve(locations)
+                        })
                     }
+                },(reject)=>{
+                    err({'error':'Error at server side'})
+                    console.log('Some error occured in fetching data from server !'+reject);
+                    
+                }
 
-                )
+            )
 
-            })
-
-
-        }
-
-function exploit(){
-       let loading=$('#loading');
-       console.log(loading)
-        let searchBar = $('#locSearch')[0];
-        console.log(searchBar)
-        let searchBtn = $('#searchBtn');
-        let locUl = $('#locUl')[0];
-        searchBtn.on('click', () => {
-            loading.css('display','block')
-            if (searchBar.value != undefined) {
-                searchLoc(searchBar.value).then((res) => {
-                    gen.genList(res, locUl).then((res)=>{
-                       if(res==true){
-                      loading.css('display','none')
-                       }
-                    })
-
-
-                })
-
-            } else
-                console.log('fdfd');
         })
 
+
     }
-    }
+}
 
 )
 
+/* Class contains methods for data fetching from apixu api */
 
+class FutureCast {
 
-class FutureCast{
-    
-    constructor(cityId,initialURL,key){
-        this.cityId=cityId;
-this.initialURL=initialURL;
-this.key=key;
-this.api=creds.Keys()["12HourApi"];
+    constructor(cityId, initialURL, key) {
+        this.cityId = cityId;
+        this.initialURL = initialURL;
+        this.key = key;
+        this.api = Creds.Keys()["12HourApi"];
     }
 
     /* For next 12 hours forecast */
 
-     castRes(){
+    castRes() {
 
-let url=`${this.initialURL}${this.api}?key=${this.key}&q=${this.cityId}`;
+        let url = `${this.initialURL}${this.api}?key=${this.key}&q=${this.cityId}`;
 
-        return new Promise((outResolve)=>{
+        return new Promise((outResolve) => {
 
-            fetch(url).then((resolve)=>{
-                if (resolve.status==200){
-                resolve.json().then((res)=>{
-outResolve(res);
+            fetch(url).then((resolve) => {
+                if (resolve.status == 200) {
+                    resolve.json().then((res) => {
+                        outResolve(res);
 
-                })
+                    })
 
                 }
-                
-                
-                
-                })
+            })
 
         })
-        
-       
-
 
     }
-weekCast(callback){
+    weekCast(callback) {
 
-/* For weeklyForCast */
-}
-
-
+        /* For weeklyForCast */
+    }
 }
