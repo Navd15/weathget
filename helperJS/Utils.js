ad58d570412c01cc1c5b0142a97bf60fa55a28ed
Navd15 <gill.222navdeep@gmail.com>
2018-11-03 22:51:44 +0530

    fix:changed checkIDB method to return data localStorage instead of idb

--- helperJS/Utils.js
+++ helperJS/Utils.js
@@ -1,25 +1,11 @@
 class Utils {
 
-    static checkIDB=()=> {
-       return new Promise((res, rej) => {
-         const db=   indexedDB.open('weathCache-v2',1);
-         db.onupgradeneeded=(event)=>{
-             if(event.oldVersion==0){
-                event.target.result.createObjectStore('locations', { keyPath: 'name' });
-            rej();
-             }
-             
-         }
-                db.onsuccess = (event) => {
-                    if(event.old)
-                    event.target.result
-                    .transaction('locations', 'readwrite')
-                    .objectStore('locations').getAll().onsuccess = (event) => { res(event.target.result) }
-                }
-        })
-
-
-
+    static checkIDB(callback) {
+        let places = []
+        for (let place in localStorage) {
+            places.push(place);
+        }
+        callback(places);
     }
 
 
