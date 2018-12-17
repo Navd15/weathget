2b79c2b4c3b83bf7c7d580e920211a85d6bc5ad2
Navd15 <gill.222navdeep@gmail.com>
2018-12-18 03:05:57 +0530

    feat:Added code for template literals and building new response

--- helperJS/Info.js
+++ helperJS/Info.js
@@ -3,11 +3,27 @@ class fill {
         
         /* The require  statement will not work outside the class so use it here*/
         let fs = require('fs');
-
-        fs.readFile('./views/main.html', 'utf8', (err, data) => {
-
+        let p=require('path')
+const dates={
+"1":"Jan",
+"2":"Feb",
+"3":"Mar",
+"4":"Apr",
+"5":"May",
+"6":"June",
+"7":"July",
+"8":"Aug",
+"9":"Sep",
+"10":"Oct",
+"11":"Nov",
+"12":"Dec"
+}
+       const path=p.join(__dirname,'/views','/main.html')
+       console.log(result);
+         
+        fs.readFile(path, 'utf8', (err, data) => {
             let src=result.hourCond;
-
+            const forecast=result.currentCond.forecast.forecastday
             let date=new Date();
             let d=`${date.toDateString().split(" ")[1]} ${date.getDate()},${date.getFullYear()}`;
 
@@ -23,6 +39,32 @@ class fill {
                 wind:src.current.wind_kph,
                 cloud:src.current.cloud
             };
+
+        let makeDate=(str)=>{
+const arr=str.split('-');
+return `${arr[2]} ${dates[arr[1]]} ${arr[0]}`;
+        }
+const one={
+date:makeDate(forecast[1].date),
+rain:`${forecast[1].day['totalprecip_in']}`,
+temp:`${forecast[1].day['mintemp_c']}`,
+wind:`${forecast[1].day['maxwind_kph']}`,
+text:`${forecast[1].day.condition['text']}`
+}
+
+const two={
+    date:makeDate(forecast[2].date),
+rain:`${forecast[2].day['totalprecip_in']}`,
+temp:`${forecast[2].day['mintemp_c']}`,
+wind:`${forecast[2].day['maxwind_kph']}`,
+text:`${forecast[2].day.condition['text']}`
+}
+
+const three={   date:makeDate(forecast[3].date),
+    rain:`${forecast[3].day['totalprecip_in']}`,
+    temp:`${forecast[3].day['mintemp_c']}`,
+    wind:`${forecast[3].day['maxwind_kph']}`,
+    text:`${forecast[3].day.condition['text']}`}
             data = data.toString();
     
             /* This code sinppet will convert data string to template literal */
